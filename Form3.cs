f74c9d3fc934632afc53f7caa4b2f0bc4c65f4f3
Wellesley HCI Lab <wellesleyhcilab@gmail.com>
2013-10-31 14:34:53 -0400

    Phidget Interface Kit Lab

---
+++ Form3.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+using Phidgets;         //needed for the interfacekit class and the phidget exception class
+using Phidgets.Events;
+using System.Drawing;
+using System.Windows.Forms;
+
+//using Graphics;
+namespace InterfaceKit_full
+{
+    public partial class Form3 : Form
+    {
+        private InterfaceKit ifKit;
+        private Shape[] circleArray;
+        Graphics g;
+
+        public Form3()
+        {
+            InitializeComponent();
+            BackColor = Color.Black;
+            g = this.CreateGraphics();
+            
+            circleArray = new Shape[8];
+            initCircleArray();
+           
+           
+
+        }
+
+        public void initCircleArray(){
+            //For all possible sensors in the Phidget Interface
+            for (int i = 0; i < 8; i++)
+            {
+                // For each sensor, create an initial circle shape
+                Point p = new Point(100, 100);
+                circleArray[i] = new Shape("ellipse", Color.Red, p, g);
+                circleArray[i].changeSize(new Point(300, 300));
+                
+                // Place shape somewhere on the form
+                if (i < 4)
+                {
+                    circleArray[i].changePosition(new Point((i + 1) * 50, (i+1)*50));
+                }
+                else
+                {
+                    circleArray[i].changePosition(new Point((i + 1) * 50, 0));
+                }
+
+                //Draw the shapes
+                // If you input the string "gradient",any gradient changes will be applied
+                circleArray[i].Draw("normal");
+            }// end for loop
+
+
+        }
+        
+        //Initialize the device
+        public void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //IfKit attach event handler
+        public void ifKit_Attach(object sender, AttachEventArgs e)
+        {
+            BackColor = Color.Black;
+        }
+
+        //Ifkit detach event handler
+        public void ifKit_Detach(object sender, DetachEventArgs e)
+        {
+            BackColor = Color.White;
+        }
+
+        //Sensor input change event handler
+        public void ifKit_SensorChange(object sender, SensorChangeEventArgs e)
+        {
+           
+                if (e.Index==0)
+                {
+                    circleArray[e.Index].changeColor(Color.FromArgb(e.Value * 1 / 4, e.Value * 1 / 4, e.Value * 1 / 4));
+                    circleArray[e.Index].changeSize(new Point((e.Value) + 1, (e.Value) + 1));
+                }   
+                   
+
+            
+                g.Clear(Color.Black);
+                
+                for (int i = 0; i < 8; i++)
+                {
+                    // Draw (with gradients)
+                    circleArray[i].Draw("");
+                   
+                 }
+        }
+
+
+    }
+}
+
---
+++ Shape.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing.Drawing2D;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace InterfaceKit_full
+{
+    class Shape
+    {
+        private Point pnt;
+        private Pen drawPen = new Pen(Color.Black);
+        private SolidBrush fillBrush;
+        private LinearGradientBrush gradBrush;
+        private Rectangle rect;
+        private Graphics g;
+        private ShapeType type;
+
+        enum ShapeType
+        {
+            rectangle,
+            ellipse
+        }
+
+        public Shape(string shapeType, Color c, Point pntXY, Graphics inputG)
+        {
+            pnt = pntXY;
+            makeShapeType(shapeType.ToLower(), pntXY);
+            fillBrush=new SolidBrush(c);
+            gradBrush=new LinearGradientBrush(rect,c,c,0,true);
+            g = inputG;
+
+        }
+
+        private void makeShapeType(string s, Point p)
+        {
+            switch (s)
+            {
+                case "rectangle":
+                    type = ShapeType.rectangle;
+                    rect = new Rectangle(new Point(1, 1), new Size(p));
+                    break;
+                case "ellipse":
+                    type = ShapeType.ellipse;
+                    rect = new Rectangle(new Point(1, 1), new Size(p));
+                    break;
+                default:
+                    type = ShapeType.rectangle;
+                    rect = new Rectangle(new Point(1, 1), new Size(p));
+                    break;
+            }
+        }
+
+        public void changePosition(Point p)
+        {
+            rect.Location = p;
+        }
+
+        public void changeSize(Point s)
+        {
+            rect.Size = (Size)s;
+        }
+
+        public void changeColor(Color c)
+        {
+            fillBrush = new SolidBrush(c);
+        }
+        public void changeColorGrad(Color b, Color c)
+        {
+            gradBrush = new LinearGradientBrush(rect,b,c, 1, true);
+        }
+        public void Draw(String s)
+        {
+
+            switch (type)
+            {
+                case ShapeType.rectangle:
+                    if(s.Equals("gradient"))
+                        g.FillRectangle(gradBrush,rect);
+                    else
+                        g.FillRectangle(fillBrush,rect);
+                    g.DrawRectangle(drawPen,rect);
+                    break;
+                case ShapeType.ellipse:
+                    if(s.Equals("gradient"))
+                        g.FillEllipse(gradBrush,rect);
+                    else
+                        g.FillEllipse(fillBrush,rect);
+                    g.DrawEllipse(drawPen,rect);
+                    break;
+
+            }
+        }
+
+    }
+}
